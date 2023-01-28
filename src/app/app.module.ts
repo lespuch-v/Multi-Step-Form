d1bc493c4c93bd2aceafcc4b509c714b78e8babb
Vasyl Lespuch <v.lespukh@gmail.com>
2023-01-28 17:37:20 +0100

    working on the step two

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { FormStepOneComponent } from './form-step-one/form-step-one.component';
+import {AppComponent} from './app.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {FormStepOneComponent} from './form-step-one/form-step-one.component';
+import {FormStepTwoComponent} from './form-stap-two/form-step-two.component';
+
+const routes: Routes = [
+  {path: 'component1', component: FormStepOneComponent},
+  {path: 'component2', component: FormStepTwoComponent},
+  { path: '', redirectTo: '/component1', pathMatch: 'full' }
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    FormStepOneComponent
+    FormStepOneComponent,
+    FormStepTwoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/form-stap-two/form-step-two.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-stap-two',
+  templateUrl: './form-step-two.component.html',
+  styleUrls: ['./form-step-two.component.css']
+})
+export class FormStepTwoComponent {
+
+}
